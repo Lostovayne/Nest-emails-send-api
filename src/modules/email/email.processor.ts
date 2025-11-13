b81d28b592884524329094dc988a5291cfa1a97b
Deus lo Vult <92962731+Lostovayne@users.noreply.github.com>
2025-11-13 03:55:23 -0300

    Implement email processing with job handling for sending emails

--- src/modules/email/email.processor.ts
+++ src/modules/email/email.processor.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Process, Processor } from '@nestjs/bull';
+import type { Job } from 'bull';
+import { SendEmailDto } from 'src/common/dtos';
+import { createTransport } from 'src/config/nodemailer';
 
-@Injectable()
-export class EmailProcessor {}
+@Processor('email-queue')
+export class EmailProcessor {
+  @Process('send-email')
+  async handleSendEmail(job: Job<SendEmailDto>) {
+    const { smtpConfig, mailOptions } = job.data;
+    const transporter = createTransport(smtpConfig);
+
+    try {
+      await transporter.sendMail(mailOptions);
+    } catch (error) {
+      throw new Error(`Failed to send email: ${error}`);
+    }
+  }
+}
