c2c1a3d1396b64b63c70cfe9f98c48ca6bac2a5f
Deus lo Vult <92962731+Lostovayne@users.noreply.github.com>
2025-11-13 04:08:15 -0300

    Add Redis service to Docker Compose and sanitize HTML in email processor

--- src/modules/email/email.processor.ts
+++ src/modules/email/email.processor.ts
@@ -1,5 +1,6 @@
 import { Process, Processor } from '@nestjs/bull';
 import type { Job } from 'bull';
+import sanitizeHtml from 'sanitize-html';
 import { SendEmailDto } from 'src/common/dtos';
 import { createTransport } from 'src/config/nodemailer';
 
@@ -8,10 +9,16 @@ export class EmailProcessor {
   @Process('send-email')
   async handleSendEmail(job: Job<SendEmailDto>) {
     const { smtpConfig, mailOptions } = job.data;
+
+    // Sanitize HTML content to prevent XSS attacks
+    const sanitizedHtml = sanitizeHtml(mailOptions.html);
     const transporter = createTransport(smtpConfig);
 
     try {
-      await transporter.sendMail(mailOptions);
+      await transporter.sendMail({
+        ...mailOptions,
+        html: sanitizedHtml,
+      });
     } catch (error) {
       throw new Error(`Failed to send email: ${error}`);
     }
